e3d4ca4c5d95a857987dbc6489903c3666754eac
AriyaMo81 <mokhtarpourariya95@gmail.com>
2025-03-06 23:09:49 +0330

    create a simple crud api

--- server.js
+++ server.js
@@ -1,9 +1,52 @@
 /** @format */
 
 const express = require("express");
+const config = require("./config");
+
+let users = require("./users");
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+global.config = require("./config");
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(express.urlencoded({ extended: false }));
+
+// app.get("/:username", (req, res) => {
+//   console.log(req.params);
+//   res.send(`hlloworld`);
+// });
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    data: users,
+    succes: true,
+  });
+});
+
+app.get("/:id", (req, res) => {
+  let user = users.find((user) => {
+    if (user.id == req.params.id) {
+      return user;
+    }
+  });
+  res.status(200).json({
+    data: user,
+    succes: true,
+  });
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  req.body.id = parseInt(req.body.id);
+  users.push(req.body);
+  res.json({
+    data: "user added",
+    succes: true,
+  });
+});
+
+app.listen(config.port, () => {
+  console.log(`server is running on port ${config.port}`);
 });
---
+++ users.js
@@ -0,0 +1,124 @@
+/** @format */
+
+module.exports = [
+  {
+    id: 1,
+    username: "ali_reza",
+    email: "ali.reza@example.com",
+    phonenumber: "09121234567",
+  },
+  {
+    id: 2,
+    username: "sara_king",
+    email: "sara.king@example.com",
+    phonenumber: "09129876543",
+  },
+  {
+    id: 3,
+    username: "mehdi_dev",
+    email: "mehdi.dev@example.com",
+    phonenumber: "09351239876",
+  },
+  {
+    id: 4,
+    username: "fatemeh_art",
+    email: "fatemeh.art@example.com",
+    phonenumber: "09219875643",
+  },
+  {
+    id: 5,
+    username: "hamed_music",
+    email: "hamed.music@example.com",
+    phonenumber: "09135428765",
+  },
+  {
+    id: 6,
+    username: "zahra_star",
+    email: "zahra.star@example.com",
+    phonenumber: "09365428765",
+  },
+  {
+    id: 7,
+    username: "mohammad_coder",
+    email: "mohammad.coder@example.com",
+    phonenumber: "09234567891",
+  },
+  {
+    id: 8,
+    username: "elham_writer",
+    email: "elham.writer@example.com",
+    phonenumber: "09127895432",
+  },
+  {
+    id: 9,
+    username: "navid_sport",
+    email: "navid.sport@example.com",
+    phonenumber: "09236547892",
+  },
+  {
+    id: 10,
+    username: "reza_gamer",
+    email: "reza.gamer@example.com",
+    phonenumber: "09124567891",
+  },
+  {
+    id: 11,
+    username: "narges_chef",
+    email: "narges.chef@example.com",
+    phonenumber: "09324598761",
+  },
+  {
+    id: 12,
+    username: "hossein_tech",
+    email: "hossein.tech@example.com",
+    phonenumber: "09136847592",
+  },
+  {
+    id: 13,
+    username: "shahram_artist",
+    email: "shahram.artist@example.com",
+    phonenumber: "09329876541",
+  },
+  {
+    id: 14,
+    username: "amir_teacher",
+    email: "amir.teacher@example.com",
+    phonenumber: "09213456789",
+  },
+  {
+    id: 15,
+    username: "samira_model",
+    email: "samira.model@example.com",
+    phonenumber: "09129876542",
+  },
+  {
+    id: 16,
+    username: "ehsan_photographer",
+    email: "ehsan.photo@example.com",
+    phonenumber: "09354637281",
+  },
+  {
+    id: 17,
+    username: "leila_dancer",
+    email: "leila.dance@example.com",
+    phonenumber: "09237894561",
+  },
+  {
+    id: 18,
+    username: "yasin_traveler",
+    email: "yasin.travel@example.com",
+    phonenumber: "09127894563",
+  },
+  {
+    id: 19,
+    username: "parisa_scientist",
+    email: "parisa.science@example.com",
+    phonenumber: "09321458769",
+  },
+  {
+    id: 20,
+    username: "milad_programmer",
+    email: "milad.pro@example.com",
+    phonenumber: "09235647891",
+  },
+];
